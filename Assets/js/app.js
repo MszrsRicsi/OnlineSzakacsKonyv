a77f17a2e704d1994b8c2361d0f2921890df0945
MszrsRicsi <78954828+MszrsRicsi@users.noreply.github.com>
2024-09-18 08:26:38 +0200

    Login és Register html-ek

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,3 +2,8 @@ async function render(view){
     let main = document.querySelector('main');
     main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
 }
+
+function logOut()
+{
+    
+};
